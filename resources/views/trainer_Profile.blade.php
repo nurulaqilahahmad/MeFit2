65700b6b8509b1d7c42555aba9ee70ba0dd361ab
Nurul Aqilah Ahmad <naqilahahmad@yahoo.com>
2022-01-26 03:06:06 +0800

    training session add button

--- resources/views/trainer_Profile.blade.php
+++ resources/views/trainer_Profile.blade.php
@@ -137,7 +137,7 @@
 
               <!--Training Session-->
               <div class="tab-pane fade pt-3" id="training-session">
-              <a href="trainer_AddTrainingSession" class="getstarted scrollto"><span>Add Training Session</span></a>
+              <a class="getstarted scrollto" href="trainer_AddTrainingSession"><span>Add Training Session</span></a>
               @foreach ($trainingSession as $trainingSession)
                 <h5 class="card-title">{{$trainingSession->trainingSessionName}}</h5>
 
