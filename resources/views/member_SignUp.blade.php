c85b1f4762f759d2ff95752fcce2045f6712ed23
Nurul Aqilah Ahmad <naqilahahmad@yahoo.com>
2022-01-27 04:49:13 +0800

    sign up

--- resources/views/member_SignUp.blade.php
+++ resources/views/member_SignUp.blade.php
@@ -43,7 +43,7 @@
         <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
 
             <a href="/" class="logo d-flex align-items-center">
-            <img src="assets/img/logoMF.png" alt="">
+                <img src="assets/img/logoMF.png" alt="">
                 <span>MeFit</span>
             </a>
 
@@ -65,61 +65,86 @@
 
     <!-- ======= Hero Section ======= -->
     <section id="hero" class="hero d-flex align-items-center">
-
         <div class="container">
             <div class="row">
-                <div class="col-lg-6 d-flex flex-column justify-content-center">
-                    <p data-aos="fade-up">SIGN UP AS MEMBER</p>
-                    <form id="login" method="post" action="/memberCreate">
-                        @if(Session::has('success'))
-                        <div data-aos="fade-up" class="alert alert-success">{{Session::get('success')}}</div>
-                        @endif
-                        @if(Session::has('fail'))
-                        <div data-aos="fade-up" class="alert alert-danger">{{Session::get('fail')}}</div>
-                        @endif
-                        @csrf
-                        <label data-aos="fade-up"><b>Fullname</b></label>
-                        <br>
-                        <input data-aos="fade-up" type="text" name="memberFullname" id="memberFullname" placeholder="Fullname" required="true">
-                        <br>
+                <div class="col-lg-6 d-flex flex-column justify-content-center" data-aos="fade-up">
 
-                        <label data-aos="fade-up"><b>Email</b></label>
-                        <br>
-                        <input data-aos="fade-up" type="text" name="memberEmail" id="memberEmail" placeholder="Email" required="true">
-                        <br>
-
-                        <label data-aos="fade-up"><b>Username</b></label>
-                        <br>
-                        <input data-aos="fade-up" type="text" name="memberUsername" id="memberUsername" placeholder="Username" required="true">
-                        <br>
-
-                        <label data-aos="fade-up"><b>Telephone No.</b></label>
-                        <br>
-                        <input data-aos="fade-up" type="text" name="memberTelno" id="memberTelno" placeholder="Telephone No." required="true">
-                        <br>
-
-                        <label data-aos="fade-up"><b>Password</b></label>
-                        <br>
-                        <input data-aos="fade-up" type="password" name="memberPassword" id="memberPassword" placeholder="Password" required="true">
-                        <br><br>
-
-                        <input data-aos="fade-up" type="submit" id="log" value="Create Account">
-                        <br>
-
-                        <span data-aos="fade-up">Already have an account? <a data-aos="fade-up" href="member_SignIn">Sign In</a> </span>
-                        <br>
-                    </form>
-
+                    <div class="reply-form" data-aos="fade-up">
+                        <h4>SIGN UP AS MEMBER</h4>
+                        <form id="login" method="post" action="/memberCreate">
+                            @if(Session::has('success'))
+                            <div data-aos="fade-up" class="alert alert-success">{{Session::get('success')}}</div>
+                            @endif
+                            @if(Session::has('fail'))
+                            <div data-aos="fade-up" class="alert alert-danger">{{Session::get('fail')}}</div>
+                            @endif
+                            @csrf
+                            <div class="row">
+                                <div class="col form-group">
+                                    <label data-aos="fade-up" class="justify-content-center"><b>Fullname</b></label>
+                                    <input data-aos="fade-up" name="memberFullname" type="text" class="form-control" placeholder="Fullname">
+                                    <br>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col form-group">
+                                    <label data-aos="fade-up" class="justify-content-center"><b>Email</b></label>
+                                    <input data-aos="fade-up" name="memberEmail" type="text" class="form-control" placeholder="Email">
+                                    <br>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col form-group">
+                                    <label data-aos="fade-up" class="justify-content-center"><b>Username</b></label>
+                                    <input data-aos="fade-up" name="memberUsername" type="text" class="form-control" placeholder="Username">
+                                    <br>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col form-group">
+                                    <label data-aos="fade-up" class="justify-content-center"><b>Telephone No.</b></label>
+                                    <input data-aos="fade-up" name="memberTelno" type="text" class="form-control" placeholder="Telephone No.">
+                                    <br>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col form-group">
+                                    <label data-aos="fade-up" class="justify-content-center"><b>Password</b></label>
+                                    <input data-aos="fade-up" name="memberPassword" type="password" class="form-control" placeholder="Password">
+                                    <br>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col form-group">
+                                    <input data-aos="fade-up" class="justify-content-center" type="submit" id="log" value="Create Account">
+                                    <br>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col form-group">
+                                    <br>
+                                    <span data-aos="fade-up"> First Time? <a data-aos="fade-up" href="member_SignUp">Sign Up</a> </span>
+                                    <br>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col form-group">
+                                    <br>
+                                    <span data-aos="fade-up">Already have an account? <a data-aos="fade-up" href="member_SignIn">Sign In</a> </span>
+                                    <br>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-lg-6 hero-img" data-aos="zoom-out" data-aos-delay="200">
+                    <img src="assets/img/Gym.png" class="img-fluid" alt="">
                 </div>
             </div>
         </div>
-        <div class="col-lg-6 hero-img" data-aos="zoom-out" data-aos-delay="200">
-            <img src="{{url('assets/img/Gym.png')}}" class="img-fluid" alt="">
-        </div>
-        </div>
-        </div>
 
     </section><!-- End Hero -->
+    
     <!-- {{url('assets/img/SignUp.png')}} -->
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
