87afc2c32c688f9c94c1454b4baeaeec6d4e75d0
Nurul Aqilah Ahmad <naqilahahmad@yahoo.com>
2022-01-27 05:48:59 +0800

    trainer profile

--- resources/views/member_TrainerProfile.blade.php
+++ resources/views/member_TrainerProfile.blade.php
@@ -82,19 +82,13 @@
     <section class="section profile">
         <div class="row">
             <div class="col-xl-4">
-
                 <div class="card">
                     <div class="card-body profile-card pt-4 d-flex flex-column align-items-center">
-
                         <img class="rounded-circle" src="{{$trainer->trainerImage}}" alt="Profile Picture">
                         <br>
                         <h2>{{$trainer->trainerFullname}}</h2>
-                        <div class="social-links mt-2">
-
-                        </div>
                     </div>
                 </div>
-
             </div>
 
             <div class="col-xl-8">
