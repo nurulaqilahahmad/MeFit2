34cf9065d0fc036727067212e2d961034b6741cc
Nurul Aqilah Ahmad <naqilahahmad@yahoo.com>
2022-01-27 04:50:23 +0800

    sign up

--- resources/views/member_SignUp.blade.php
+++ resources/views/member_SignUp.blade.php
@@ -83,55 +83,47 @@
                                 <div class="col form-group">
                                     <label data-aos="fade-up" class="justify-content-center"><b>Fullname</b></label>
                                     <input data-aos="fade-up" name="memberFullname" type="text" class="form-control" placeholder="Fullname">
-                                    <br>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col form-group">
                                     <label data-aos="fade-up" class="justify-content-center"><b>Email</b></label>
                                     <input data-aos="fade-up" name="memberEmail" type="text" class="form-control" placeholder="Email">
-                                    <br>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col form-group">
                                     <label data-aos="fade-up" class="justify-content-center"><b>Username</b></label>
                                     <input data-aos="fade-up" name="memberUsername" type="text" class="form-control" placeholder="Username">
-                                    <br>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col form-group">
                                     <label data-aos="fade-up" class="justify-content-center"><b>Telephone No.</b></label>
                                     <input data-aos="fade-up" name="memberTelno" type="text" class="form-control" placeholder="Telephone No.">
-                                    <br>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col form-group">
                                     <label data-aos="fade-up" class="justify-content-center"><b>Password</b></label>
                                     <input data-aos="fade-up" name="memberPassword" type="password" class="form-control" placeholder="Password">
-                                    <br>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col form-group">
                                     <input data-aos="fade-up" class="justify-content-center" type="submit" id="log" value="Create Account">
-                                    <br>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col form-group">
                                     <br>
                                     <span data-aos="fade-up"> First Time? <a data-aos="fade-up" href="member_SignUp">Sign Up</a> </span>
-                                    <br>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col form-group">
                                     <br>
                                     <span data-aos="fade-up">Already have an account? <a data-aos="fade-up" href="member_SignIn">Sign In</a> </span>
-                                    <br>
                                 </div>
                             </div>
                         </form>
@@ -144,7 +136,7 @@
         </div>
 
     </section><!-- End Hero -->
-    
+
     <!-- {{url('assets/img/SignUp.png')}} -->
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
