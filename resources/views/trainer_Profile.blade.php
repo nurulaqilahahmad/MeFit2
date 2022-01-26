2fcbe535ad0615e49a2c3dff499895491d1eb6e0
Nurul Aqilah Ahmad <naqilahahmad@yahoo.com>
2022-01-26 09:13:55 +0800

    add button

--- resources/views/trainer_Profile.blade.php
+++ resources/views/trainer_Profile.blade.php
@@ -140,7 +140,6 @@
                   <a href="trainer_AddTrainingSession">
                     <button class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">Add Training Session<button>
                   </a>
-              <a class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center" href="trainer_AddTrainingSession"><span>Add Training Session</span></a>
               </div>
               @foreach ($trainingSession as $trainingSession)
                 <h5 class="card-title">{{$trainingSession->trainingSessionName}}</h5>
