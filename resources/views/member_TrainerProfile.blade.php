16e80a927fd0c3a5b3f045fb36bdcaccbaf0c6ff
Nurul Aqilah Ahmad <naqilahahmad@yahoo.com>
2022-01-27 03:35:31 +0800

    member training list

--- resources/views/member_TrainerProfile.blade.php
+++ resources/views/member_TrainerProfile.blade.php
@@ -48,9 +48,9 @@
 
             <nav id="navbar" class="navbar">
                 <ul>
-                    <li><a class="nav-link scrollto" href="member_Index#hero">Home</a></li>
-                    <li><a class="nav-link scrollto" href="member_Index#pricing">Membership Plan</a></li>
-                    <li><a class="nav-link scrollto" href="member_Index#team">Training Session</a></li>
+                    <li><a class="nav-link scrollto" href="{{route('member_Index#hero')}}">Home</a></li>
+                    <li><a class="nav-link scrollto" href="{{route('member_Index#pricing')}}">Membership Plan</a></li>
+                    <li><a class="nav-link scrollto" href="{{route('member_Index#team')}}">Training Session</a></li>
                     <li class="dropdown"><a href="#"><span>{{$member->memberUsername}}</span> <i class="bi bi-chevron-down"></i></a>
                         <ul>
                             <li><a href="member_Profile">Profile</a></li>
@@ -70,8 +70,8 @@
     <section class="breadcrumbs">
         <div class="container">
             <ol>
-                <li><a href="member_Index#pricing">Home</a></li>
-                <li><a href="member_TrainingSessionList">Training Session</a></li>
+                <li><a href="{{route('member_Index#pricing')}}">Home</a></li>
+                <li><a href="{{route('member_TrainingSessionList')}}">Training Session</a></li>
                 <li>Profile</li>
             </ol>
             <h2>{{$trainer->trainerFullname}}</h2>
