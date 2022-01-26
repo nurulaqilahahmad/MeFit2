814edd98eaa6cd4c9b3aa8ad62fd75262b86b3bf
Nurul Aqilah Ahmad <naqilahahmad@yahoo.com>
2022-01-27 03:38:18 +0800

    change

--- resources/views/member_TrainerProfile.blade.php
+++ resources/views/member_TrainerProfile.blade.php
@@ -48,9 +48,9 @@
 
             <nav id="navbar" class="navbar">
                 <ul>
-                    <li><a class="nav-link scrollto" href="{{route('/member_Index#hero')}}">Home</a></li>
-                    <li><a class="nav-link scrollto" href="{{route('/member_Index#pricing')}}">Membership Plan</a></li>
-                    <li><a class="nav-link scrollto" href="{{route('/member_Index#team')}}">Training Session</a></li>
+                    <li><a class="nav-link scrollto" href="{{route('/member_Index')}}">Home</a></li>
+                    <li><a class="nav-link scrollto" href="{{route('/member_Index')}}">Membership Plan</a></li>
+                    <li><a class="nav-link scrollto" href="{{route('/member_TrainingSessionList')}}">Training Session</a></li>
                     <li class="dropdown"><a href="#"><span>{{$member->memberUsername}}</span> <i class="bi bi-chevron-down"></i></a>
                         <ul>
                             <li><a href="member_Profile">Profile</a></li>
